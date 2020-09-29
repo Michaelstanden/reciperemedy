c5255a365b8d32d0f94661677c4bed4516f3ba47
Michaelstanden <michaelstanden@hotmail.com>
2020-09-29 14:16:30 +0000

    added basic template structure using bootstrap and materialize for index, breakfast and base templates.

--- app.py
+++ app.py
@@ -1,15 +1,37 @@
-import os
-from flask import Flask
+import os 
+import flask_pymongo
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from os import path
+if path.exists("env.py"):
+    import env 
 
+SECRET_KEY = os.environ.get('MONGO_URI')
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'Recipes'
+app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
+
+mongo = PyMongo(app)
+
 
 @app.route('/')
-def hello():
-    return 'Hello world'
+@app.route('/get_recipes')
+def get_recipes():
+    return render_template("recipe.html", recipes=mongo.db.Recipes.find())
 
 
+@app.route('/')
+@app.route('/home')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/breakfast')
+def breakfast():
+    return render_template('breakfast.html')
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
-    port=int(os.environ.get('PORT')),
-    debug=True)
+            port=int(os.environ.get('PORT')),
+            debug=True)
